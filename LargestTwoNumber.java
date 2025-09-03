049e80305b7812fcfa4a6ac51d64557e3612b06b
Gulshankumar987 <gulshan.kumar_bca24@gla.ac.in>
2025-09-03 11:13:38 +0530

    largest two number

---
+++ LargestTwoNumber.java
@@ -0,0 +1,12 @@
+public class LargestTwoNumber {
+  public static void main (String arg[]){
+    int a =10;
+    int b= 20;
+    if (a>b){
+      System.out.println("Largest  number is ");
+
+    }else {
+      System.out.println("Smallest of number");
+    }
+  }
+}
